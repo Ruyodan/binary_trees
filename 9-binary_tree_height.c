d36293036f3016490dc45725ff29934fffd9d3c4
Ruyodan <salvator.hamdoune@gmail.com>
2024-02-28 11:55:41 -0800

    Updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,15 +12,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/* If the tree is NULL, the height is 0 */
 	if (tree == NULL)
 	{
-	return (0);
-	}
+	size_t l = 0, r = 0;
 
 	/* Calculate the height of the left subtree */
-	size_t left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
 	/* Calculate the height of the right subtree */
-	size_t right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	/* Return the maximum height of the two subtrees */
-	return (left_height > right_height ? left_height : right_height);
+	return ((l > r) ? l : r);
+
+	}
+	return (0);
 }
