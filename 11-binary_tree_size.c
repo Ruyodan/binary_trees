2a4cc72f7181484a2b5485d175d2f593bacf4a30
Ruyodan <salvator.hamdoune@gmail.com>
2024-02-28 14:16:16 -0800

    Updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,22 +6,19 @@
  *
  * Return: The size of the binary tree. If the tree is NULL, returns 0.
  *
- * Description: This function measures the size of a binary tree, which is the
- * total number of nodes in the tree. The size is calculated by recursively
- * counting the nodes in the left and right subtrees and adding 1 for the root.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	/* If the tree is NULL, the size is 0 */
-	if (tree == NULL)
+	size_t size = 0;
+
+	if (tree)
 	{
-		return (0);
-	}
 
 	/* Recursively count the size of the left and right subtrees */
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
-
+	size += 1;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	}
 	/* Size of the tree is the sum of the sizes of the left and right subtrees */
-	return (left_size + right_size + 1);
+	return (size);
 }
